a8dde665e15029415ef3683a2218aa5eef5b5d3c
Shayan Najd <sh.najd@gmail.com>
2015-06-06 12:19:15 +0100

    removing old code

--- QFeldspar/Type/GADT.hs
+++ QFeldspar/Type/GADT.hs
@@ -130,52 +130,6 @@ getPrfHasSinMayM :: HasSin Typ (Maybe ta) =>
                    t (Maybe ta) -> ErrM (PrfHasSin Typ ta)
 getPrfHasSinMayM = return  . getPrfHasSinMay
 
-type family Out (t :: *) :: * where
-  Out (ta -> tb) = Out tb
-  Out t          = t
-
-data EqlOut :: * -> * -> * where
-  EqlOut :: EqlOut (Out t2) t2
-
-eqlOut :: Typ t1 -> Typ t2 -> ErrM (EqlOut t1 t2)
-eqlOut Wrd         Wrd           = return EqlOut
-eqlOut Bol         Bol           = return EqlOut
-eqlOut Flt         Flt           = return EqlOut
-eqlOut t           (Arr _ tb)    = do EqlOut <- eqlOut t tb
-                                      return EqlOut
-eqlOut (Ary ta)    (Ary ta')     = do Rfl <- eqlSin ta ta'
-                                      return EqlOut
-eqlOut (Vct ta)    (Vct ta')     = do Rfl <- eqlSin ta ta'
-                                      return EqlOut
-eqlOut (May ta)    (May ta')     = do Rfl <- eqlSin ta ta'
-                                      return EqlOut
-eqlOut (Tpl tf ts) (Tpl tf' ts') = do Rfl <- eqlSin tf tf'
-                                      Rfl <- eqlSin ts ts'
-                                      return EqlOut
-eqlOut Cmx         Cmx           = return EqlOut
-eqlOut _              _          = fail "Normalisation Error!"
-
-type family Arg (t :: *) :: [*] where
-  Arg (ta -> tb) = ta ': Arg tb
-  Arg t          = '[]
-
-data EqlArg :: [*] -> * -> * where
-  EqlArg :: EqlArg (Arg t2) t2
-
-eqlArg :: ET.Env Typ r -> Typ t -> ErrM (EqlArg r t)
-eqlArg ET.Emp         Wrd          = return EqlArg
-eqlArg ET.Emp         Bol          = return EqlArg
-eqlArg ET.Emp         Flt          = return EqlArg
-eqlArg (ET.Ext ta ts) (Arr ta' tb) = do Rfl    <- eqlSin ta ta'
-                                        EqlArg <- eqlArg ts tb
-                                        return EqlArg
-eqlArg ET.Emp         (Ary _)      = return EqlArg
-eqlArg ET.Emp         (Vct _)      = return EqlArg
-eqlArg ET.Emp         (May _)      = return EqlArg
-eqlArg ET.Emp         (Tpl _ _)    = return EqlArg
-eqlArg ET.Emp         Cmx          = return EqlArg
-eqlArg _              _            = fail "Normalisation Error!"
-
 mapC :: Types as =>
         (forall a. Type a => f a -> f' a) ->
         ET.Env f as -> ET.Env f' as
@@ -220,71 +174,6 @@ getVecTyp (Vct ta) = ta
 getMayTyp :: Typ (Maybe ta) -> Typ ta
 getMayTyp (May ta) = ta
 
-getOutTyp :: forall a. Typ a -> Typ (Out a)
-getOutTyp (Arr _ b) = getOutTyp b
-getOutTyp a         = case obvious :: a :~: Out a of
-  Rfl              -> a
-
-prfHasSinOut :: forall t. PrfHasSin Typ t -> PrfHasSin Typ (Out t)
-prfHasSinOut PrfHasSin = case sin :: Typ t of
-  Wrd       -> PrfHasSin
-  Bol       -> PrfHasSin
-  Flt       -> PrfHasSin
-  Arr ta tb -> case (getPrfHasSin ta , getPrfHasSin tb) of
-    (PrfHasSin , y@PrfHasSin) -> prfHasSinOut y
-  Tpl tf ts -> case (getPrfHasSin tf , getPrfHasSin ts) of
-    (PrfHasSin , PrfHasSin) -> PrfHasSin
-  Ary ta    -> case getPrfHasSin ta of
-    PrfHasSin -> PrfHasSin
-  Vct ta    -> case getPrfHasSin ta of
-    PrfHasSin -> PrfHasSin
-  May ta    -> case getPrfHasSin ta of
-    PrfHasSin -> PrfHasSin
-  Cmx       -> PrfHasSin
-
-getTypTailEnv :: forall t ra rb tf.
-                 (Arg t ~ Add ra rb) =>
-                 Typ t -> ET.Env tf ra -> ET.Env tf rb -> ExsSin Typ
-getTypTailEnv t          ET.Emp        _ = ExsSin t
-getTypTailEnv (Arr _ ts) (ET.Ext _ rs) r = getTypTailEnv ts rs r
-getTypTailEnv _          _             _ = impossible
-
-getSinDiff :: (Arg t ~ Add ra rb) =>
-              Typ t -> ET.Env tf ra -> ET.Env tf rb -> ET.Env Typ rb
-getSinDiff _          _             ET.Emp          = ET.Emp
-getSinDiff (Arr _ t)  (ET.Ext _ ra) rb@(ET.Ext _ _) = getSinDiff t ra rb
-getSinDiff (Arr t ts) ET.Emp        (ET.Ext _ rb)   = ET.Ext t (getSinDiff ts ET.Emp rb)
-getSinDiff _          _             _               = impossible
-
-getPrf :: (Arg t ~ Add ra (tb ': rb)) =>
-          Typ t -> ET.Env tf ra -> ET.Env tf (tb ': rb) -> PrfHasSin Typ tb
-getPrf Wrd                   _          _           = impossible
-getPrf Bol                   _          _           = impossible
-getPrf Flt                   _          _           = impossible
-getPrf (Tpl _ _)             _          _           = impossible
-getPrf (Ary _)               _          _           = impossible
-getPrf (Vct _)               _          _           = impossible
-getPrf Cmx                   _          _           = impossible
-getPrf (May _)               _          _           = impossible
-getPrf (Arr t Wrd)       ET.Emp        (ET.Ext _ ET.Emp) = getPrfHasSin t
-getPrf (Arr _ Wrd)       _          _           = impossible
-getPrf (Arr t Bol)       ET.Emp        (ET.Ext _ ET.Emp) = getPrfHasSin t
-getPrf (Arr _ Bol)       _          _           = impossible
-getPrf (Arr t Flt)       ET.Emp        (ET.Ext _ ET.Emp) = getPrfHasSin t
-getPrf (Arr _ Flt)       _          _           = impossible
-getPrf (Arr t (Tpl _ _)) ET.Emp        (ET.Ext _ ET.Emp) = getPrfHasSin t
-getPrf (Arr _ (Tpl _ _)) _          _           = impossible
-getPrf (Arr t (Ary _))   ET.Emp        (ET.Ext _ ET.Emp) = getPrfHasSin t
-getPrf (Arr _ (Ary _))   _          _           = impossible
-getPrf (Arr t (Vct _))   ET.Emp        (ET.Ext _ ET.Emp) = getPrfHasSin t
-getPrf (Arr _ (Vct _))   _          _           = impossible
-getPrf (Arr t Cmx)       ET.Emp        (ET.Ext _ ET.Emp) = getPrfHasSin t
-getPrf (Arr _ Cmx)       _          _           = impossible
-getPrf (Arr t (May _))   ET.Emp        (ET.Ext _ ET.Emp) = getPrfHasSin t
-getPrf (Arr _ (May _))   _          _           = impossible
-getPrf (Arr t (Arr _ _)) ET.Emp        (ET.Ext _ _)   = getPrfHasSin t
-getPrf (Arr _ ts@(Arr _ _)) (ET.Ext _ es) es'      = getPrf ts es es'
-
 type family (:->) a b where
   '[]       :-> b = b
   (a ': as) :-> b = a -> (as :-> b)
@@ -293,7 +182,7 @@ cur :: ET.Env Typ as -> Typ a -> Typ (as :-> a)
 cur ET.Emp        b = b
 cur (ET.Ext a as) b = Arr a (cur as b)
 
-getPrfHasSinEnv :: forall a as. HasSin (ET.Env Typ) (a ': as) => (PrfHasSin Typ a, PrfHasSin (ET.Env Typ) as)
+getPrfHasSinEnv :: forall a as. Types (a ': as) => (PrfHasSin Typ a, PrfHasSin (ET.Env Typ) as)
 getPrfHasSinEnv = case sin :: ET.Env Typ (a ': as) of
   ET.Ext a as  -> (getPrfHasSin a , getPrfHasSin as)
 
